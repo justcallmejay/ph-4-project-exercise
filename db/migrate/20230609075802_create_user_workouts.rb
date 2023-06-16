c921a98636dc83b59a0febefae138324c2d52c70
jay <jaypifiverr@gmail.com>
2023-06-16 04:39:04 -0400

    Add timer attribute for isometric exercises

---
+++ db/migrate/20230609075802_create_user_workouts.rb
@@ -0,0 +1,16 @@
+class CreateUserWorkouts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_workouts do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :workout, null: false, foreign_key: true
+      t.integer :reps
+      t.integer :weight
+      t.float :percent_completed
+      t.float :intensity
+      t.integer :timer
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
