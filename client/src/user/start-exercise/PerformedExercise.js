823e783196f9d874e238e697d397c36fbaaf3b50
jay <jaypifiverr@gmail.com>
2023-06-16 04:41:58 -0400

    Displays timer values onMouseOver event

--- client/src/user/start-exercise/PerformedExercise.js
+++ client/src/user/start-exercise/PerformedExercise.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { ImCheckmark } from 'react-icons/im';
 import { RiDeleteBin2Fill } from 'react-icons/ri'
+import { LuTimer } from 'react-icons/lu'
 
 import './PerformedExercise.css'
 
 function PerformedExercise( { perEx, index, deleteLastRoutine, arrayLength } ) {
 
     const [ toggleDelete, setToggleDelete ] = useState(false)
+    const [ toggleTimer, setToggleTimer ] = useState(false)
+
+    console.log(perEx)
 
     function onMouseEnter() {
         if (index === arrayLength - 1)
@@ -17,14 +21,30 @@ function PerformedExercise( { perEx, index, deleteLastRoutine, arrayLength } ) {
         setToggleDelete(false)
     }
 
-    console.log(perEx)
+    function handleToggleTimer() {
+        setToggleTimer(true)
+    }
+
+    function handleToggleTimerLeave() {
+        setToggleTimer(false)
+    }
+
     return(
         <div className='performed-exercise fl' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
             <div className='performed-exercise-attr fl'>
-                <h6>{perEx.workout.name}: ({perEx.workout.kind})</h6> |
-                <h6>Weight: {perEx.weight}</h6> |
-                <h6>Reps: {perEx.reps}</h6> |
-                <h6>Completed: {perEx.percent_completed === 100 ? <ImCheckmark/> : perEx.percent_completed }</h6>
+                <h6 title={`[${perEx.workout.muscle}] ${perEx.workout.name} (${perEx.workout.kind})`}>[{perEx.workout.muscle}] {perEx.workout.name}: ({perEx.workout.kind})</h6> |
+                <h6>Wt: {perEx.weight}</h6> |
+                {toggleTimer ? 
+                <h6>Timer: {perEx.timer} sec.</h6> :
+                <h6>Reps: {perEx.reps}</h6> } 
+                {perEx.timer ? 
+                    <div className='timer-container rc' onMouseEnter={handleToggleTimer} onMouseLeave={handleToggleTimerLeave}>
+                        <LuTimer />
+                    </div> 
+                        : ""}
+                |
+                <h6>RPE: {perEx.intensity}</h6> |
+                <h6>{perEx.percent_completed >= 100 ? <ImCheckmark/> : perEx.percent_completed.toFixed(1) }</h6>
             </div>
             {toggleDelete ? 
             <div className='delete-container cc' onClick={() => deleteLastRoutine(index, perEx.id)}>
