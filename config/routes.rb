46fed68765bd88d76e2516a67872fb608210270c
jay <jaypifiverr@gmail.com>
2023-06-17 10:05:29 -0400

    Add path to find number of users that did an exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  resources :user_workouts, only: [:index, :create, :destroy]
+  resources :workouts, only: [:index]
+  resources :users, only: [:create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post 'login', to: 'sessions#create'
+  get '/authorized', to: 'users#show'
+  get '/get_exercises', to: 'workouts#get_exercises'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/find_by_date', to: 'users#find_by_date'
+  get '/get_user_counts', to:'workouts#get_user_counts'
 
   # Defines the root path route ("/")
   # root "articles#index"
