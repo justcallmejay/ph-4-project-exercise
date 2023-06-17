c11acc0fef1c60c7ecf9ae10ef45b95204955e5f
jay <jaypifiverr@gmail.com>
2023-06-17 10:05:47 -0400

    Add path to find number of users that did an exercise

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,20 @@
+class WorkoutsController < ApplicationController
+
+    wrap_parameters format: []
+
+    def index
+        workout = Workout.all
+        render json: workout, status: :ok
+    end
+
+    def get_exercises
+        workout = Workout.where(muscle: params[:muscle])
+        render json: workout, status: :ok
+    end
+
+    def get_user_counts
+        workout = Workout.find_by(id: params[:id]).users.uniq
+        render json: workout.length, status: :ok
+    end
+
+end
