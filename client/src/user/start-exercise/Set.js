8c437aff88d2b2cb5f379918758a794f109933df
jay <jaypifiverr@gmail.com>
2023-06-20 13:09:45 -0400

    Add radio buttons to allow user to switch between bodyweight measurement and additional wt for bodyweight exercises

--- client/src/user/start-exercise/Set.js
+++ client/src/user/start-exercise/Set.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Timer from './Timer';
 import { HiOutlineInformationCircle } from 'react-icons/hi'
+import { BsQuestion } from 'react-icons/bs'
 import './Set.css'
 
-function Set( { seconds, setSeconds, selectExercise, formData, handleChange, setEraseInput, eraseInput, errors, handleToggleDisplay } ) {
+function Set( { seconds, setSeconds, selectExercise, formData, handleChange, setEraseInput, eraseInput, errors, handleToggleDisplay, currentUser } ) {
+
+    const numbers = ['7', '8', '8.5', '9', '9.5', '10', '10.5']
+
+
+    const [ toggleDisplay, setToggleDisplay ] = useState(false)
+    const [ radioSelect, setRadioSelect ] = useState(null)
 
     function handleEraseCheckBox(e) {
         setEraseInput(e.target.checked)
     }
+
+    function toggleExplanation() {
+        setToggleDisplay(toggleDisplay => !toggleDisplay)
+    }
+
+    const defaultLimit = selectExercise.kind === 'Bodyweight' ? currentUser.weight : 0
+    const defaultMax = selectExercise.kind === 'Bodyweight' ? currentUser.weight : 1000
+
+
+    const readOnly = formData.weight === currentUser.weight && selectExercise.kind === 'Bodyweight' && radioSelect === 'bw' ? true : false
 
     return(
         <>
             <div className='selected-exercise-goal rc'>
                 <div className='selected-exercise-fields fl'>
                     <h5>{selectExercise.name} ({selectExercise.kind})</h5>
+                    {selectExercise.kind === 'Bodyweight' ? 
+                    <>
+                    <input 
+                        type='radio' 
+                        value='bw' 
+                        checked={radioSelect === 'bw'} 
+                        onChange={() => setRadioSelect('bw')}
+                    />
+                    <h6>BW</h6>
+                    <input 
+                        type='radio' 
+                        value='weight' 
+                        checked={radioSelect === 'weight'} 
+                        onChange={() =>  setRadioSelect('weight')}
+                    />
+                    <h6>wt.</h6>
+                    </>
+                     : ""}
                 </div>
                 <div className='selected-exercise-fields-input fl'>
-                    <h5>Weight:</h5>
-                    <input className='start-exercise-input' name='weight' type='number' min='0' max='2000' value={formData.weight} onChange={handleChange}/>
+                    <h5>Weight:</h5>{selectExercise.kind === 'Bodyweight' ? <BsQuestion onClick={toggleExplanation}/> : ""}
+                    {radioSelect === 'bw' ?
+                    toggleDisplay ? 
+                    <div className='weight-display-container fl'>
+                        <h6>BW is a reference to your bodyweight.  Click up or down to enter weight.</h6><button onClick={toggleExplanation}>Close</button>
+                    </div> : ""
+                     :
+                    toggleDisplay ? 
+                     <div className='weight-display-container fl'>
+                        <h6>wt. refers to additional weight used when performing the exercise (eg: weight vest)</h6><button onClick={toggleExplanation}>Close</button>
+                    </div> : ""
+                     }
+                    <input className='start-exercise-input' name='weight' type='number' min={radioSelect === 'bw' ? defaultLimit : 0} max={radioSelect === 'bw' ? defaultMax : 1000} value={formData.weight} onChange={handleChange} readOnly={readOnly}/>
                     <h5>Reps:</h5>
                     <input className='start-exercise-input' name='reps' type='number' min='0' max='100' value={formData.reps} onChange={handleChange}/>
                 </div>
@@ -39,19 +85,19 @@ function Set( { seconds, setSeconds, selectExercise, formData, handleChange, set
                         <h5>RPE:</h5><HiOutlineInformationCircle onClick={handleToggleDisplay}/>
                         <select name='intensity' onChange={handleChange}>
                             <option value=''></option>
-                            <option value='7'>7</option>
-                            <option value='8'>8</option>
-                            <option value='8.5'>8.5</option>
-                            <option value='9'>9</option>
-                            <option value='9.5'>9.5</option>
-                            <option value='10'>10</option>
-                            <option value='10.5'>10.5</option>
+                            {numbers.map(num =>
+                            <option value={num}>{num}</option>
+                            )}
                         </select>
                     </div>
                 </div>
                 <div className='selected-exercise-submit fl'>
                     {errors ? 
-                    <h6>{errors.intensity}</h6> : ""}
+                    <>
+                    <h6>{errors.intensity}</h6>
+                    <h6>{errors.percent_completed}</h6>
+                    </>
+                    : ""}
                     <button>Submit</button>
                 </div>
             </> 
