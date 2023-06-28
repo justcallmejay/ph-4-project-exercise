3868eb21003c09a84612016507189d13f9077813
justcallmejay <106436963+justcallmejay@users.noreply.github.com>
2023-06-28 17:45:57 -0400

    Update

--- bin/exercise-build.sh
+++ bin/exercise-build.sh
@@ -7,8 +7,8 @@ rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
-/usr/share/rvm/gems/ruby-2.7.4/bin/bundle install
-# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
+bundle install
+# bundle exec rake assets:precompile # These lines are commented out because we have an API-only app
 # bundle exec rake assets:clean
-/usr/share/rvm/gems/ruby-2.7.4/bin/bundle exec rake db:migrate
-/usr/share/rvm/gems/ruby-2.7.4/bin/bundle exec rake db:seed
+bundle exec rake db:migrate
+bundle exec rake db:seed
